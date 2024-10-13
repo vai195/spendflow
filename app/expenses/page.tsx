717c9f28423478aeedb34946984f53402c19024c
vai195 <vksonnakul@gmail.com>
2024-10-13 10:10:34 -0400

    add cool homepage affect

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -34,6 +34,17 @@ async function Expensespage() {
           </Card>
         )}
       </div>
+      {/* <div className='mt-8'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 800 200'
+          className='w-full h-auto'>
+          <path
+            fill='#000'
+            d='M0 100 L50 50 L100 70 L150 40 L200 60 L250 30 L300 80 L350 30 L400 80 L450 50 L500 70 L550 20 L600 80 L650 30 L700 50 L750 40 L800 100 L800 200 L0 200 Z'
+          />
+        </svg>
+      </div> */}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,34 @@ export default function Home() {
   if (userId) redirect("/expenses");
   return (
     <div className='flex flex-col items-center justify-center gap-5 mt-5'>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      <div className='particle'></div>
+      {/* <div className='pulsing-circle circle1'></div>
+      <div className='pulsing-circle circle2'></div>
+      <div className='pulsing-circle circle3'></div>
+      <div className='pulsing-circle circle4'></div>
+      <div className='pulsing-circle circle5'></div>
+      <div className='pulsing-circle circle6'></div>
+      <div className='pulsing-circle circle7'></div>
+      <div className='pulsing-circle circle8'></div>
+      <div className='pulsing-circle circle9'></div>
+      <div className='pulsing-circle circle10'></div> */}
+      {/* <div className="wave"></div> */}
       <div className='flex flex-col items-center gap-4 text-center'>
         <Image src={logo} alt='Spend Flow Logo' width={200} height={200} />
         <span className='font-extrabold tracking-tight text-2xl lg:text-4xl'>
           Welcome to SpendFlow AI
         </span>
       </div>
-      <p className='max-w-prose text-center '>
+      <p className='max-w-prose text-center neon-text'>
         Effortlessly manage your finances with SpendFlow AI, a smart expense
         tracker that uses AI to streamline budgeting, track spending, and
         optimize your financial flow.
@@ -31,7 +52,7 @@ export default function Home() {
         </Link>
       </Button>
       <Link href='https://github.com/vai195/spendflow'>
-        <Github />
+        <Github className='neon-text' />
       </Link>
     </div>
   );
